90e0207cf45181468f60cec45c49229e5c0ab932
vatchans <vatchans@gmail.com>
2023-01-06 21:00:43 +0530

    Frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Login from './Login';
+import Dashboard from './Dashboard';
+import Resetpassword from './Reset-password';
+import Accesstoken from './Access-token';
+import Newpassword from './Newpassword';
+import Returntologin from './Returntologin';
+import Signup from './Signup';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Login/>}/>
+    <Route path='/signup' element={<Signup/>}/>
+    <Route path="/Dashboard" element={<Dashboard/>}/>
+    <Route path="/Reset" title="Reset password" element={<Resetpassword/>}/>
+    <Route path="/auth" element={<Accesstoken/>}/>
+    <Route path="/New" element={<Newpassword/>}/>
+    <Route path='/return' element={<Returntologin/>}/>
+      </Routes>
+      <ToastContainer/>
+  </BrowserRouter>
+
 }
 
 export default App;
