e3c31d2e5cc3046fd329babb1f35507a4af258e6
vatchans <112924038+vatchans@users.noreply.github.com>
2023-01-06 21:30:17 +0530

    Update Emailutli.js

--- public/utlis/Emailutli.js
+++ public/utlis/Emailutli.js
@@ -4,7 +4,7 @@ function generateEmail(auth=""){
     <h4 style="text-align:center;border:1px solid silver;width:fit-content;padding:8px;margin-left:40%; border-radius:5px";>${auth}</h4>
     <p>Enter this access code for authorization and don’t worry! You can use the following button to reset your password</p>
    <button style="width:fit-content;height:40px;color:white;background-color:#08bd3e;margin-left:30%;padding:10px;border: none;border-radius:8px;">
-    <a href="http://localhost:3000/auth" style="text-decoration:none;color:white">Reset your Password</a></button>
+    <a href="https://dapper-duckanoo-011852.netlify.app/auth" style="text-decoration:none;color:white">Reset your Password</a></button>
     </div>`
 }
 module.exports={generateEmail};
